5a7045e233f80653330cf519f24718906c58615a
Elektron1c97 <Elektron1c97@gmail.com>
2015-09-11 09:33:46 +0200

    Implementation of length validator

--- activerecord/test/cases/validations/length_validation_test.rb
+++ activerecord/test/cases/validations/length_validation_test.rb
@@ -40,4 +40,20 @@ class LengthValidationTest < ActiveRecord::TestCase
     assert o.valid?
   end
 
+  def test_validates_size_of_reprects_records_marked_for_destruction
+    assert_nothing_raised { Owner.validates_size_of :pets, minimum: 1 }
+    owner = Owner.new
+    assert_not owner.save
+    assert owner.errors[:pets].any?
+    pet = owner.pets.build
+    assert owner.valid?
+    assert owner.save
+
+    pet_count = Pet.count
+    assert_not owner.update_attributes pets_attributes: [ {_destroy: 1, id: pet.id} ]
+    assert_not owner.valid?
+    assert owner.errors[:pets].any?
+    assert_equal pet_count, Pet.count
+  end
+
 end
--- activerecord/test/models/owner.rb
+++ activerecord/test/models/owner.rb
@@ -2,4 +2,6 @@ class Owner < ActiveRecord::Base
   self.primary_key = :owner_id
   has_many :pets, -> { order 'pets.name desc' }
   has_many :toys, :through => :pets
+
+  accepts_nested_attributes_for :pets, :allow_destroy => true
 end
